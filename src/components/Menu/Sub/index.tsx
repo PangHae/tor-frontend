a751f6b91c0bf798fa0c89cd429e86989b87e852
PangHae <yds05074@gmail.com>
2022-08-23 02:22:11 +0900

    feat : MainPage 변경에 따른 Props 변경
    - Category API 연결 완료하면 로직 변경 필요

--- src/components/Menu/Sub/index.tsx
+++ src/components/Menu/Sub/index.tsx
@@ -3,19 +3,16 @@ import { IoSearchSharp } from 'react-icons/io5';
 import Button from 'src/components/base/Button';
 import Input from 'src/components/base/Input';
 import CategoryList from 'src/components/Category/List';
+
 import styles from './subMenu.module.scss';
 
-interface Props {
-  categoryList: string[];
-}
-
-function SubMenu({ categoryList }: Props): React.ReactElement {
+function SubMenu(): React.ReactElement {
   const [randCategory, setRandCategory] = useState<string[]>([]);
   const [clickedCategory, setClickedCategory] = useState('');
 
   useEffect(() => {
     // 카테고리 랜덤하게 출력하도록 변경해야함
-    setRandCategory([categoryList[0], categoryList[1], categoryList[2], categoryList[3]]);
+    setRandCategory(['김치', '라면', '김밥', '수제비']);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
