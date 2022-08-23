0a0dd61f77e59970a7465ae92f2a101396f7c9c1
PangHae <yds05074@gmail.com>
2022-08-23 17:49:49 +0900

    feat : preset category button api 연결

--- src/components/Menu/Sub/index.tsx
+++ src/components/Menu/Sub/index.tsx
@@ -3,18 +3,29 @@ import { IoSearchSharp } from 'react-icons/io5';
 import Button from 'src/components/base/Button';
 import Input from 'src/components/base/Input';
 import CategoryList from 'src/components/Category/List';
+import useAxios from 'src/hooks/useAxios';
 
 import styles from './subMenu.module.scss';
 
 function SubMenu(): React.ReactElement {
   const [randCategory, setRandCategory] = useState<string[]>([]);
   const [clickedCategory, setClickedCategory] = useState('');
+  const { fetchData: getPresetCategories, res: getPresetCategoriesRes } = useAxios({
+    method: 'get',
+    url: '/api/category/getAllPresetCategories',
+  });
 
   useEffect(() => {
     // 카테고리 랜덤하게 출력하도록 변경해야함
-    setRandCategory(['김치', '라면', '김밥', '수제비']);
+    getPresetCategories(null);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (getPresetCategoriesRes) {
+      setRandCategory(getPresetCategoriesRes.content.map((preCat: any) => preCat.categoryName));
+    }
+  }, [getPresetCategoriesRes]);
 
   const handleOnChangeCategory: ChangeEventHandler<HTMLInputElement> = (e) => {
     const { target } = e;
