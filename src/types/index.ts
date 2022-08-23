f6f6bc5373488f996042289803db69b163b7c21e
cotton <bmtosss@gmail.com>
2022-08-23 20:18:29 +0900

    chore: 타입 선언

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,10 @@
 import PresetType from './preset';
 import ProductType from './product';
+import CartActionType from './cartAction';
 
 export type CategoryNProduct = {
   categoryName: string[];
   product: ProductType[];
 };
 
-export type { PresetType, ProductType };
+export type { PresetType, ProductType, CartActionType };
