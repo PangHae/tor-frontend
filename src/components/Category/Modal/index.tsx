3ec290fbe837179ffcb72b009a9d19da1a8235be
PangHae <yds05074@gmail.com>
2022-08-23 15:53:07 +0900

    feat : PresetCreate 카테고리 조회 기능 추가

--- src/components/Category/Modal/index.tsx
+++ src/components/Category/Modal/index.tsx
@@ -1,8 +1,7 @@
-import React, { ChangeEvent, ReactElement, SetStateAction, useState } from 'react';
+import React, { ReactElement, SetStateAction, useEffect } from 'react';
 import Button from 'src/components/base/Button';
-import InputTitle from 'src/components/base/InputTitle';
-import { IoSearchSharp } from 'react-icons/io5';
 import { Title } from 'src/components/base/Title';
+import useAxios from 'src/hooks/useAxios';
 
 import styles from './style.module.scss';
 
@@ -12,33 +11,39 @@ interface props {
   onClose: () => void;
 }
 
-const categories = ['라면', '김치'];
-
 function CategoryModal({
   presetCategoryList,
   setPresetCategoryList,
   onClose,
 }: props): ReactElement {
-  // category List를 미리 받아서 가지고 있다가 검색해서 있으면 보여주는 식!
-  const [searchCategory, setSearchCategory] = useState('');
-  const [buttonTag, setButtonTag] = useState('');
+  const { fetchData: getTopCategory, res: getTopCategoryRes } = useAxios({
+    method: 'get',
+    url: '/api/category/getCategories',
+  });
+  const { fetchData: getMiddleCategory, res: getMiddleCategoryRes } = useAxios({
+    method: 'get',
+    url: '/api/category/getCategories',
+  });
+  const { fetchData: getLowCategory, res: getLowCategoryRes } = useAxios({
+    method: 'get',
+    url: '/api/category/getCategories',
+  });
 
-  const handleClickSearch = () => {
-    if (categories.includes(searchCategory)) {
-      setButtonTag(searchCategory);
-    } else {
-      setButtonTag('');
-    }
+  useEffect(() => {
+    getTopCategory(null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleOnClickAddCategory = (categoryName: string) => {
+    setPresetCategoryList([...presetCategoryList, categoryName]);
   };
 
-  const handleOnChange = (e: ChangeEvent) => {
-    const { target } = e;
-    setSearchCategory((target as HTMLInputElement).value);
+  const handleClickTopCategory = (categoryName: string) => {
+    getMiddleCategory(`/${encodeURI(categoryName)}`);
   };
 
-  const handleOnClickAddCategory = (e: any) => {
-    const { target } = e;
-    setPresetCategoryList([...presetCategoryList, (target as HTMLButtonElement).innerHTML]);
+  const handleClickMiddleCategory = (categoryName: string) => {
+    getLowCategory(`/${encodeURI(categoryName)}`);
   };
 
   return (
@@ -47,23 +52,49 @@ function CategoryModal({
       <div className={styles.Modal}>
         <Title classname='BigTitle' text='카테고리 검색' />
         <div className={styles.Search}>
-          <InputTitle
-            inputClassname='LongInput'
-            title='검색'
-            placeholder='검색할 카테고리를 입력해주세요'
-            onChange={handleOnChange}
-          />
-          <Button classname='ImageButton' style={{ margin: 'auto 0' }} onClick={handleClickSearch}>
-            <IoSearchSharp width={40} height={40} color='#000' />
-          </Button>
+          <div className={styles.TopCategory}>
+            {getTopCategoryRes
+              ? getTopCategoryRes.content.map((item: any) => {
+                  return (
+                    <Button onClick={() => handleClickTopCategory(item.categoryName)}>
+                      <p>{item.categoryName}</p>
+                    </Button>
+                  );
+                })
+              : null}
+          </div>
+          <hr />
+          <div className={styles.MiddleCategory}>
+            {getMiddleCategoryRes
+              ? getMiddleCategoryRes.content.map((item: any) => {
+                  return (
+                    <Button onClick={() => handleClickMiddleCategory(item.categoryName)}>
+                      {item.categoryName}
+                    </Button>
+                  );
+                })
+              : null}
+          </div>
+          <hr />
+          <div className={styles.LowCategory}>
+            {getLowCategoryRes
+              ? getLowCategoryRes.content.map((item: any) => {
+                  return (
+                    <Button onClick={() => handleOnClickAddCategory(item.categoryName)}>
+                      {item.categoryName}
+                    </Button>
+                  );
+                })
+              : null}
+          </div>
         </div>
-        {buttonTag ? (
+        {/* {buttonTag ? (
           <Button classname='CategoryButton' onClick={handleOnClickAddCategory}>
             {buttonTag}
           </Button>
         ) : (
           <Title text='일치하는 카테고리가 없습니다' />
-        )}
+        )} */}
       </div>
     </>
   );
