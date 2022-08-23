834b13c6a95914b56b2cfac869065b5285eb3dba
cotton <bmtosss@gmail.com>
2022-08-23 20:17:31 +0900

    feat: 장바구니 전역 상태로 구현

--- src/components/Preset/CartCard/index.tsx
+++ src/components/Preset/CartCard/index.tsx
@@ -11,76 +11,35 @@ import Row from 'src/components/base/Row';
 import styles from './style.module.scss';
 
 interface props {
-  originalPreset: PresetType;
+  preset: PresetType;
   funcBind: [
     (presetId: number) => void,
-    (presetId: number) => void,
-    (price: number) => void,
-    (price: number) => void,
+    (presetId: number, productId: number, checked: boolean) => void,
+    (presetId: number, productId: number) => void,
+    (presetId: number, productId: number) => void,
   ];
 }
 
-function PresetCartCard({ originalPreset, funcBind }: props): ReactElement {
-  const [handlePresetAdd, handlePresetRemove, handleTotalPriceAdd, handleTotalPriceSub] = funcBind;
-  const [products, setProducts] = useState<ProductType[]>([...originalPreset.products!]);
-  const [checked, setChecked] = useState(true);
+function PresetCartCard({ preset, funcBind }: props): ReactElement {
+  const [onPresetCheck, onProductCheck, onProductAdd, onProductSub] = funcBind;
+  const [products, setProducts] = useState<ProductType[]>([...preset.products!]);
+  // const [checked, setChecked] = useState(true);
 
-  const handlePresetChange = (e: FormEvent<HTMLInputElement>) => {
-    if (e.currentTarget.checked) {
-      setChecked(true);
-      handlePresetAdd(originalPreset.presetId);
-    } else {
-      setChecked(false);
-      handlePresetRemove(originalPreset.presetId);
-    }
+  const handlePresetCheck = (e: FormEvent<HTMLInputElement>) => {
+    onPresetCheck(preset.presetId);
+    // setChecked(e.currentTarget.checked);
+  };
+
+  const handleProductCheck = (productId: number, checked: boolean) => {
+    onProductCheck(preset.presetId, productId, checked!);
   };
 
   const handleProductAdd = (productId: number) => {
-    setProducts(
-      [...products].map((product) => {
-        if (productId === product.productId) {
-          product.count += 1;
-          handleTotalPriceAdd(product.price);
-        }
-        return product;
-      }),
-    );
+    onProductAdd(preset.presetId, productId);
   };
+
   const handleProductSub = (productId: number) => {
-    setProducts(
-      [...products].map((product) => {
-        if (productId === product.productId) {
-          product.count -= 1;
-          handleTotalPriceSub(product.price);
-        }
-        return product;
-      }),
-    );
-  };
-
-  const handleProductChecked = (productId: number) => {
-    setProducts(
-      [...products].map((product) => {
-        if (productId === product.productId) {
-          product.checked = true;
-          handleTotalPriceAdd(product.price * product.count);
-        }
-        return product;
-      }),
-    );
-  };
-
-  const handleProductUnchecked = (productId: number) => {
-    setProducts(
-      [...products].map((product) => {
-        if (productId === product.productId) {
-          product.checked = false;
-          handleTotalPriceSub(product.price * product.count);
-        }
-        return product;
-      }),
-    );
-    setChecked(products.some((product) => product.checked));
+    onProductSub(preset.presetId, productId);
   };
 
   return (
@@ -88,25 +47,22 @@ function PresetCartCard({ originalPreset, funcBind }: props): ReactElement {
       <Row className={styles.TitleWrapper}>
         <input
           type='checkbox'
-          checked={checked}
-          onChange={handlePresetChange}
+          checked={preset.checked}
+          onChange={handlePresetCheck}
           className={styles.EntireCheckbox}
         />
-        <p className={styles.Title}>{originalPreset.presetName}</p>
+        <p className={styles.Title}>{preset.presetName}</p>
       </Row>
       <div className={styles.Horizontal} />
-      {products!.map((product) => (
-        <Row
-          className={cx(styles.ProductWrapper, !product.checked && styles.Checked)}
+      {preset.products!.map((product) => (
+        <ProductCart
+          product={product}
+          presetChecked={preset.checked}
+          funcBind={[handleProductAdd, handleProductSub]}
+          onProductCheck={handleProductCheck}
+          isCheckBoxShow
           key={product.productId}
-        >
-          <ProductCart
-            product={product}
-            funcBind={[handleProductAdd, handleProductSub]}
-            cartFuncBind={[handleProductChecked, handleProductUnchecked]}
-            isCheckBoxShow
-          />
-        </Row>
+        />
       ))}
     </Column>
   );
