1a7e6c94de764d842327c1c7f94c974ee6b804f4
PangHae <yds05074@gmail.com>
2022-08-24 11:25:07 +0900

    feat : 개인화 추천 목록 api 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useRecoilState } from 'recoil';
+import useAxios from 'src/hooks/useAxios';
 
 import ShowItems from 'src/components/ShowItems';
 import Menu from 'src/components/Menu';
@@ -13,20 +15,64 @@ import { PresetType } from 'src/types';
 import { userState } from 'src/hooks/recoil/atoms/user';
 
 interface Props {
-  presetRanking: {
-    content: PresetType[];
-  };
-  userPresetRanking: {
-    content: PresetType[];
-  };
+  presetRanking: PresetType[];
 }
 
-function Home({ presetRanking, userPresetRanking }: Props) {
+type ScoreData = {
+  preset: PresetType;
+  score: number;
+};
+
+function Home({ presetRanking }: Props) {
   const [user] = useRecoilState(userState);
-  const tabs = [
-    { tabName: '지금 가장 핫한 모음집 >', presetRanking },
-    { tabName: '이 모음집 어때요? >', presetRanking: userPresetRanking },
+  const [userPresetRanking, setUserPresetRanking] = useState<PresetType[]>([]);
+  const tabs: { tabName: string; presetList: PresetType[] }[] = [
+    { tabName: '지금 가장 핫한 모음집 >', presetList: presetRanking },
+    { tabName: '이 모음집 어때요? >', presetList: userPresetRanking },
   ];
+  const { fetchData: getRecommend, res: getRecommendRes } = useAxios({
+    method: 'get',
+    url: '/api/recommend/getUserRecommendPreset',
+  });
+
+  useEffect(() => {
+    if (user) {
+      getRecommend(`/${user.userNumber}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  useEffect(() => {
+    if (getRecommendRes && user) {
+      Promise.all(
+        getRecommendRes.content.map(async (preset: PresetType) => {
+          const scoredPreset = await axios.get(
+            `${process.env.NEXT_PUBLIC_ADDR}/api/preset/getEvalPresets/${user.userId}/${preset.presetId}`,
+          );
+          return scoredPreset;
+        }),
+      ).then((res: AxiosResponse[]) => {
+        const scorePreset = res.map((item) => {
+          return item.data;
+        });
+        scorePreset.sort((a: ScoreData, b: ScoreData) => {
+          if (a.score > b.score) {
+            return -1;
+          }
+          if (b.score > a.score) {
+            return 1;
+          }
+          return 0;
+        });
+        setUserPresetRanking(
+          scorePreset.map((item) => {
+            return item.preset;
+          }),
+        );
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getRecommendRes]);
 
   return (
     <>
@@ -37,13 +83,9 @@ function Home({ presetRanking, userPresetRanking }: Props) {
         <SearchTab />
         <Menu />
         <SubMenu />
-        {user.userName ? (
+        {user.userName && userPresetRanking ? (
           tabs.map((tab) => (
-            <ShowItems
-              tabTitle={tab.tabName}
-              presetRanking={tab.presetRanking.content}
-              key={tab.tabName}
-            />
+            <ShowItems tabTitle={tab.tabName} presetRanking={tab.presetList} key={tab.tabName} />
           ))
         ) : (
           <LoginModal />
@@ -55,14 +97,9 @@ function Home({ presetRanking, userPresetRanking }: Props) {
 
 export const getServerSideProps = async () => {
   const presetRanking = await axios.get(`${process.env.NEXT_PUBLIC_ADDR}/api/preset/getPresetRank`);
-  const userPresetRanking = await axios.get(
-    `${process.env.NEXT_PUBLIC_ADDR}/api/recommend/getUserRecommendPreset/2`,
-  );
-  // 카테고리 api 연결 필요
   return {
     props: {
-      presetRanking: presetRanking.data,
-      userPresetRanking: userPresetRanking.data,
+      presetRanking: presetRanking.data.content,
     },
   };
 };
